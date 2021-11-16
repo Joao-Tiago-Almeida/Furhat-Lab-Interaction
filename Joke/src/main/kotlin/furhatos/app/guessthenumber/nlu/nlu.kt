1d76f2befe530d56c1dcb5b57bd0ccf2f8a89744
Joao-Tiago-Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2021-11-16 15:01:26 +0100

    it ask the name to two persons

---
+++ Joke/src/main/kotlin/furhatos/app/guessthenumber/nlu/nlu.kt
@@ -0,0 +1,34 @@
+package furhatos.app.guessthenumber.nlu
+
+import furhatos.nlu.Intent
+import furhatos.util.Language
+import furhatos.nlu.*
+import furhatos.nlu.grammar.Grammar
+import furhatos.nlu.kotlin.grammar
+import furhatos.nlu.common.Number
+
+/*
+open class GetName : Intent(), TextGenerator {
+    val name : String? = null
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "@name",
+            "My name is @name",
+            "I am @name",
+            "You are speaking with @name",
+            "is my name @name"
+        )
+    }
+
+    override fun toText(lang: Language): String {
+        return generate(
+            lang,
+            "[name is $name] | [I am $name] | [speaking with $name]"
+        )
+    }
+
+    override fun toString(): String {
+        return toText()
+    }
+}
+*/
